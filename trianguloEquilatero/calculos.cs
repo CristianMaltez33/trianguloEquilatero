15deea7e0cc5c210fd504baa205782d59e71a79c
CristianMaltez <cristianjosuemaltezrivas@gmail.com>
2023-09-03 14:35:45 -0600

    Agregado método para calcular tipo de triangulo

--- trianguloEquilatero/calculos.cs
+++ trianguloEquilatero/calculos.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace trianguloEquilatero
 {
+    /// <summary>
+    /// Calcula si un triangulo es equilatero o no, en base a valor de todos sus lados
+    /// </summary>
     class calculos
     {
     }
