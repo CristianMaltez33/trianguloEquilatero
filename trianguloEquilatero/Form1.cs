59074ff45340eb269422499d1081761b425db93f
CristianMaltez <cristianjosuemaltezrivas@gmail.com>
2023-09-03 13:48:48 -0600

    Recurso para rama main

---
+++ trianguloEquilatero/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trianguloEquilatero
+{
+    public partial class FrmEquilatero : Form
+    {
+        public FrmEquilatero()
+        {
+            InitializeComponent();
+        }
+    }
+}
